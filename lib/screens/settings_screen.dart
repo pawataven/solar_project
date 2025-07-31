43054f13147c59941ac896f500ac9d505f381b23
Pawat <38448373+pawataven@users.noreply.github.com>
2025-07-31 10:31:56 +0700

    Revert "ADD: connect_DB"

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatefulWidget {
-  const SettingsScreen({super.key});
+  const SettingsScreen({Key? key}) : super(key: key);
 
   @override
   State<SettingsScreen> createState() => _SettingsScreenState();
