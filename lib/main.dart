c2a4e87519d361479ee2d395f22e6adb3ca7ffba
pawataven <pawatwantrong@gmail.com>
2025-07-31 10:29:24 +0700

    ADD: FIX-DB

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:solar_project/firebase_options.dart';
 import 'package:solar_project/services/main_ctrl.dart';
 import 'screens/home_screen.dart';
-void main() => runApp(const MyApp());
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
